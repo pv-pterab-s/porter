3055a31dca8e09af93dd94b8a3a20393e9561fab
Gallagher Donovan Pryor <gallagher@arrayfire.com>
2023-01-11 13:28:51 -0500

    run-tests.sh fails if batch-1 branch not on expected sha

--- run-tests.sh
+++ run-tests.sh
@@ -4,6 +4,12 @@ set -e
 source config.sh
 cd out/batch-1-af/build
 
+EXPECTED_SHA="ff37d59d"
+if ! [ "$(git rev-parse --short HEAD)" = "$EXPECTED_SHA" ]; then
+    echo "out/batch-1-af is expected to be at sha $EXPECTED_SHA"
+    exit 1
+fi
+
 ./test/tile_oneapi --gtest_filter="Tile/0.*:-Tile/0.Tile111:Tile/0.Tile3D111:Tile/0.Tile2D111"
 
 for I in $NEW_FUNCTIONS; do
